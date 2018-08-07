e36a0b1be9a571a7e95fa9024807fe4b2f1535ed
Graham Spencer <grahamspence@gmail.com>
2018-08-07 16:06:51 -0400

    added edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
+  get 'pictures/:id/edit' => 'pictures#edit'
+  patch 'pictures/:id' => 'pictures#update'
+
   get 'pictures/:id' => 'pictures#show'
 
 end
